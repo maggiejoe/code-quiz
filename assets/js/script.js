437a81d2e79d2f87332210b46b86842fd8f1bb98
maggiejoe <maggiejoseph94@gmail.com>
2022-03-19 12:45:15 -0400

    psuedocode made for js

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// when 'Start Quiz' button is clicked the following must happen
+// 1) the main header turns into the question
+// 2) the intro disappears
+// 3) the hidding buttons display with the 4 possible answers
+// 4) the countdown begins
+
+// ^^ repeats until all questions are gone through
+// ^^ if a question is answered wrong 10 seconds comes off the timer
+// ^^ when the user selects and answer, 'correct' or 'wrong' is displayed at the bottom of the possible answers
+
+// onces all questions are completed
+// 1) high score is displayed (time remaining)
+// 2) and input line is displayed for the user to put their name/initials
+
+// when submit button is clicked
+// 1) high score is saved in localStorage
+// 2) two buttons appear
+// 2-a) restart - bring back to beginning to start quiz again
+// 2-b) clear high score - removes high score from localStorage
+// 3) high score is displayed on page in order of highest to lowest (top to bottom)
